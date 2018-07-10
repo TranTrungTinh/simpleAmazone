4035a277426328a150b393515356a13ceeb6cf45
Tran Trung Tinh <trantrungtinh4954@gmail.com>
2018-07-10 19:53:03 +0700

    config request and data service & message component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,15 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
-
+/*import component */
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { MessageComponent } from './message/message.component';
+/*import service */
+import { RequestService } from './services/request.service';
+import { DataService } from './services/data.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    MessageComponent
   ],
   imports: [
     BrowserModule,
@@ -18,7 +23,7 @@ import { HomeComponent } from './home/home.component';
     FormsModule,
     NgbModule.forRoot()
   ],
-  providers: [],
+  providers: [RequestService, DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ client/src/app/services/data.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Router, NavigationStart } from '@angular/router';
+
+@Injectable()
+export class DataService {
+
+  message = '';
+  messageType = 'danger';
+  user: any;
+
+  constructor(private router: Router) { 
+    this.router.events.subscribe(e => {
+      if(event instanceof NavigationStart) {
+        this.message = '';
+      }
+    });
+  }
+
+  error(message: string) {
+    this.message = message;
+    this.messageType = 'danger';
+  }
+
+  success(message: string) {
+    this.message = message;
+    this.messageType = 'success';
+  }
+
+  warning(message: string) {
+    this.message = message;
+    this.messageType = 'warning';
+  }
+}
